bbf9460737b7ad8b35404517d0a85477ba10a758
Batchaev Alan <alan.batchaev@ax-team.corp>
2023-11-14 15:52:44 +0300

    some refactory

--- js/dz-6.js
+++ js/dz-6.js
@@ -1,15 +1,15 @@
 const isPerfect = (num) => {
-  let result = [1];
+  let result = 1;
+
   for (let i = 2; i < Math.pow(num, 0.5); i++) {
-    if (num % i == 0) {
-      result.push(i);
-      result.push(num / i);
+    if (num % i === 0) {
+      result += i + num / i;
     }
   }
-  result = result.reduce((acc, curr) => (acc += curr), 0);
+
   return result === num;
 };
 
-const testNumber = 7;
+const testNumber = 28;
 
 console.log(testNumber, isPerfect(testNumber));
