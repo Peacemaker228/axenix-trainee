21d23e637650f6fa74cd7a85ad6bb9f51b524b0b
Batchaev Alan <alan.batchaev@ax-team.corp>
2023-11-14 12:56:26 +0300

    some refactory

--- js/dz-3.js
+++ js/dz-3.js
@@ -1,17 +1,22 @@
 const isHappyNumber = (num) => {
   let res = 0;
   let index = 0;
+
   const sumOfSquareDigits = (inner) => {
     res = 0;
     index += 1;
+
     for (const el of String(inner)) {
       res += el ** 2;
     }
+
     if (index < 10) {
       sumOfSquareDigits(res);
     }
+
     return res === 1;
   };
+
   return sumOfSquareDigits(num);
 };
 
