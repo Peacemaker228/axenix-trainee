42c30798cab7c3ae0e0ff6276702a78a7d370735
Batchaev Alan <alan.batchaev@ax-team.corp>
2023-11-16 14:54:44 +0300

    task OBJ-DZ-8

---
+++ js/index.js
@@ -0,0 +1,9 @@
+import { default as scrabble } from "./OBJ-DZ-8.js";
+
+console.log(scrabble("scriptingjava", "JavaScript")); // true);
+
+console.log(
+  scrabble("avjafff", "java") // true
+);
+
+console.log(scrabble("avj", "java")); // false);
