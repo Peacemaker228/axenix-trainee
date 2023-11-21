c4d1f8c2a4c6392d5368524bbca9dfba0f2f1266
Batchaev Alan <alan.batchaev@ax-team.corp>
2023-11-21 23:15:56 +0300

    tasks dz-8 update

--- js/dz-8.js
+++ js/dz-8.js
@@ -11,6 +11,20 @@ const fizzBuzz = (begin, end) => {
     } else {
       console.log(i);
     }
+    // switch (true) {
+    //   case i % 15 === 0:
+    //     console.log("FizzBuzz");
+    //     break;
+    //   case i % 3 === 0:
+    //     console.log("Fizz");
+    //     break;
+    //   case i % 5 === 0:
+    //     console.log("Buzz");
+    //     break;
+    //   default:
+    //     console.log(i);
+    //     break;
+    // }
   }
 };
 
