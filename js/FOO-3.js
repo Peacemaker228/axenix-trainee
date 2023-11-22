47a041c74f81adca554fbef9dd43efd9a8844e1c
Killer <alanchik99@gmail.com>
2023-11-23 01:22:19 +0300

    tasks FOO-3

---
+++ js/FOO-3.js
@@ -0,0 +1,29 @@
+let sides = {
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+};
+
+console.log((2.5).toFixed(0));
+
+const displayHistogram = (rollCount, foo) => {
+  const sideCount = [...Array(rollCount).keys()]
+    .map(() => foo())
+    .reduce((acc, curr) => {
+      acc[curr] = (acc[curr] || 0) + 1;
+      return acc;
+    }, sides);
+
+  const test = Object.keys(sideCount).reduce((acc, curr) => {
+    console.log((acc[curr] * 100) / rollCount, 'def');
+    acc[curr] = _.round((acc[curr] * 100) / rollCount);
+    return acc;
+  }, sideCount);
+
+  return test;
+};
+
+export default displayHistogram;
---
+++ js/index.js
@@ -0,0 +1,8 @@
+import displayHistogram from './FOO-3.js';
+
+const rollDie = () => {
+  return Math.floor(Math.random() * 6) + 1;
+};
+
+console.log(displayHistogram(32, rollDie));
+// console.log(displayHistogram(13, rollDie));
