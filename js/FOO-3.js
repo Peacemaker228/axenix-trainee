1787fbfbb0c71c8c34de79d882c7a7c4581c7bbc
Killer <alanchik99@gmail.com>
2023-11-26 20:39:15 +0300

    tasks FOO-3 is done

--- js/FOO-3.js
+++ js/FOO-3.js
@@ -1,4 +1,4 @@
-let sides = {
+const sides = {
   1: 0,
   2: 0,
   3: 0,
@@ -7,25 +7,65 @@ let sides = {
   6: 0,
 };
 
-console.log((2.5).toFixed(0));
-
 const percent = 100;
+const space = ' ';
+const bigSpace = '   ';
+const sharp = '###';
+const dashed = '-----------------------';
+const sideNumbers = ' 1   2   3   4   5   6 ';
 
 const displayHistogram = (rollCount, foo) => {
-  const sideCount = [...Array(rollCount).keys()]
+  const sideCount = _.range(rollCount)
     .map(() => foo())
     .reduce((acc, curr) => {
       acc[curr] = (acc[curr] || 0) + 1;
+
       return acc;
     }, sides);
 
-  const test = Object.keys(sideCount).reduce((acc, curr) => {
-    console.log((acc[curr] * percent) / rollCount, 'def');
-    acc[curr] = _.round((acc[curr] * percent) / rollCount);
-    return acc;
-  }, sideCount);
+  const max = _.max(Object.values(sideCount));
 
-  return test;
+  const maxArr = _.range(max + 1).reverse();
+
+  return maxArr
+    .map((el) => {
+      return Object.values(sideCount)
+        .map((value) => {
+          if (!value) return bigSpace;
+
+          if (value === el)
+            return `${_.round((value * percent) / rollCount)}%`.padEnd(
+              3,
+              space,
+            );
+
+          return value >= el + 1 ? sharp : bigSpace;
+
+          // if (value >= el + 1) return sharp;
+
+          // return bigSpace;
+        })
+        .join(space);
+    })
+    .concat(dashed, sideNumbers)
+    .join('\n');
 };
 
 export default displayHistogram;
+
+// switch (true) {
+//   case !value:
+//     return bigSpace;
+
+//   case value === el:
+//     return `${_.round((value * percent) / rollCount)}%`.padEnd(
+//       3,
+//       space,
+//     );
+
+//   case value >= el + 1:
+//     return sharp;
+
+//   default:
+//     return bigSpace;
+// }
